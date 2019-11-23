e02883929a030971e2acc7d0d299e4c1a1e5d891
Mohammad <mohammad.musaei@gmail.com>
2019-11-23 14:51:55 +0330

    .

--- projects/ngx-decorator/src/public-api.ts
+++ projects/ngx-decorator/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of ngx-decorator
  */
 
-export * from './lib/ngx-decorator.service';
-export * from './lib/ngx-decorator.component';
 export * from './lib/ngx-decorator.module';
